cea66a6a3498501364ed526d39feeda88d377d31
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-08-01 21:47:24 +0530

    Configuration and Data Ingestion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from ChickenDiseaseClassifier import logger
+from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to the custom logger")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} Started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} Completed <<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/ChickenDiseaseClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from ChickenDiseaseClassifier.components.data_ingestion import DataIngestion
+from ChickenDiseaseClassifier.config.configuration import ConfigurationManager
+from ChickenDiseaseClassifier import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_zip()
+        data_ingestion.extract_zip_file()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> Stage {STAGE_NAME} Started <<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>> Stage {STAGE_NAME} Completed <<<<")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
